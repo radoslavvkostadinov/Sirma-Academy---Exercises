ca1f30c3080ef06bae95a708880c4572984a4d83
Radoslav Kostadinov <bounslay@gmail.com>
2024-07-29 13:13:02 +0300

    Some changes

--- Prototype and Inheritance - Exercise/03-task.js
+++ Prototype and Inheritance - Exercise/03-task.js
@@ -2,92 +2,24 @@
 // "Human" and a toSpeciesString() method that returns a string in the format "I am a
 {/* <species> . <toString>" */ }
 
-// function classFunc(classHere) {
-//     classHere.prototype.species = "Human";
-//     classHere.prototype.toSpeciesString = function () {
-//         return "I am a " + this.species + " . " + this.toString();
-//     };
-// }
-
-
-// class Person {
-//     constructor(name) {
-//         this.name = name;
-//     }
-
-//     toString() {
-//         return this.name;
-//     }
-// }
-
-// // Apply classFunc to the Person class
-// classFunc(Person);
-
-// // Create an instance of Person
-// const person = new Person("John");
-
-// console.log(person.toSpeciesString());
-// console.log(person.species);
-// console.log(person);
-
-
-// Function to add species property and toSpeciesString method
-
-function addSpeciesMixin(targetClass) {
-    targetClass.prototype.species = "Human";
-    targetClass.prototype.toSpeciesString = function () {
-        return `I am a ${this.species}. ${this.toString()}`;
-    };
+function classManager(classHere) {
+    classHere.prototype.species = 'Human';
+    classHere.prototype.toSpeciesString = function () {
+        return `I am a ${this.species} . My name is${this.name.toString()}`
+    }
 }
 
 class Person {
     constructor(name) {
         this.name = name;
     }
-
-    toString() {
-        return this.name;
-    }
 }
 
-// Apply the mixin to the Person class
-addSpeciesMixin(Person);
+classManager(Person);
 
-// Define another class with additional methods
-class Worker {
-    constructor(job) {
-        this.job = job;
-    }
+const person = new Person('John');
+console.log(person.toSpeciesString());
 
-    getJob() {
-        return this.job;
-    }
-}
-
-// Create a new class that composes Person and Worker
-class Employee {
-    constructor(name, job) {
-        this.person = new Person(name);
-        this.worker = new Worker(job);
-    }
-
-    toSpeciesString() {
-        return this.person.toSpeciesString();
-    }
-
-    getJob() {
-        return this.worker.getJob();
-    }
-
-    toString() {
-        return `${this.person.toString()} - ${this.worker.getJob()}`;
-    }
-}
-
-// Create an instance of Employee
-const employee = new Employee("John", "Engineer");
-
-console.log(employee.toSpeciesString()); // I am a Human. John
-console.log(employee.getJob()); // Engineer
-console.log(employee.toString()); // John - Engineer
-
+// Prototype chaining confirmation
+console.log(Object.getPrototypeOf(classManager) === Function.prototype); // true
+console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype); // true
